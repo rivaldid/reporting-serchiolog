a18989c587aed0203918cf8ed22c651e39104e1a
manzogi9 <manzogi9@posteitaliane.it>
2013-10-31 15:34:22 +0100

    Importate definizioni regolari da serchiolog.pl

--- Serchio.pm
+++ Serchio.pm
@@ -13,6 +13,58 @@ my $dbh;
 my $sth;
 my $data = undef;
 
+
+# DEFINIZIONI REGOLARI
+
+my $DATA = 	qr!\d{2}/\d{2}/\d{4}!;
+my $ORA =	qr!\d{2}:\d{2}!;
+my $PULSAR =	qr!PULSAR 1|PULSAR 2!;
+my $CONCEN =	qr!\(000\)|\(001\)!;
+my $NOME =	qr![[:alpha:][:space:]]{10}!;
+my $COGNOME =	qr![[:alpha:][:space:]]{16}!;
+my $VARCO =	qr!H\(\d\d\)!;
+my $VERSO =	qr!USCITA|ENTRATA!;
+my $TESSERA =	qr!\d{8}!;
+
+my $PREAMBLE = qr!(?<giorno>$DATA)\s(?<ore>$ORA)\s(?<pulsar>$PULSAR)\s{12}(?<concen>$CONCEN)!;
+
+my $EV_TAAB = qr!(?<giorno>$DATA)\s(?<ore>$ORA)\s(?<operatore>\w{6})\s+(?<evento>Tastiera Abilitata)!;
+my $EV_ALL = qr!$PREAMBLE\s+(?<evento>Allarmi Acquisiti)(?<varchi>(?:\(H\s\d\d\))|(?:\(H:\d-\d\d\)))!;
+my $EV_TAMPER = qr!$PREAMBLE\s+(?<evento>Allarme Tamper)\s(?<varco>$VARCO)!;
+my $EV_CSTATO = qr!$PREAMBLE\s+(?<evento>Comando Cambio Stato Lettore)\s$VARCO\sABILITATO\s\.\s\[\s(?<operatore>\w{6})\s\]!;
+my $EV_CADUTA = qr!$PREAMBLE\s+(?<evento>Caduta Linea)!;
+my $EV_RICPROG = qr!$PREAMBLE\s+(?<evento>Richiesta Invio Programmazione)\s\.\s\[\s(?<operatore>\w+)\s\]!;
+my $EV_FINEPROG = qr!$PREAMBLE\s+(?<evento>Fine invio dati di programmazione)!;
+my $EV_MINPULSAR = qr!$PREAMBLE\s+(?<evento>Linea Mini Pulsar)!;
+my $EV_TESANON = qr!$PREAMBLE\s\*{8}\s(?<evento>Transito effettuato)\s\s(?<varco>$VARCO)(?<verso>$VERSO)\s(?<nominativo>.+)!;
+my $EV_LINEA = qr!(?<evento>LINEA (?:ON|OFF))!;
+
+
+my $EV_TRANS =	qr!$PREAMBLE\s(?<tessera>$TESSERA)\s(?<evento>Transito effettuato)\s\s(?<varco>$VARCO)(?<verso>$VERSO)\s(?<nominativo>.+)!;
+my $EV_NCONS =	qr!$PREAMBLE\s(?<tessera>$TESSERA)\s(?<evento>Transito non consentito)\s(?<varco>$VARCO)!;
+
+my $EV_VARNAP =	qr!$PREAMBLE\s{10}(?<evento>Varco non aperto)\s(?<varco>$VARCO)!;
+my $EV_VARNCH =	qr!$PREAMBLE\s{10}(?<evento>Varco non chiuso)\s(?<varco>$VARCO)!;
+my $EV_SCASSO =	qr!$PREAMBLE\s{10}(?<evento>Scasso varco)\s(?<varco>$VARCO)!;
+my $EV_VARCHI =	qr!$PREAMBLE\s{10}(?<evento>Varco chiuso)\s(?<varco>$VARCO)!;
+
+my $EV_TESSO =	qr!$PREAMBLE\s(?<tessera>$TESSERA)\s(?<evento>Tessera sospesa)\s(?<varco>$VARCO)!;
+my $EV_TESOR =	qr!$PREAMBLE\s(?<tessera>$TESSERA)\s(?<evento>Tessera fuori orario)\s(?<varco>$VARCO)!;
+my $EV_TESIN =	qr!$PREAMBLE\s(?<tessera>$TESSERA)\s(?<evento>Tessera inesistente)\s(?<varco>$VARCO)!;
+
+sub neat{
+	my $str = shift;
+	$str =~ s/\s+$//;
+	$str =~ s/\s+/ /g;
+	return $str;
+}
+
+sub convdate{
+	my $d = shift;
+	$d =~ m!(\d{2})/(\d{2})/(\d{4})!;
+	return "$3-$2-$1";
+}
+
 sub setdata{
 	my $d = shift;
 	if ( $d =~ /\d{4}-\d\d-\d\d/ ){
