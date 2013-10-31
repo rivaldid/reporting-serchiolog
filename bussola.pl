d2328134cc817b21b658fd0e8d51bb78b96fb922
manzogi9 <manzogi9@posteitaliane.it>
2013-10-31 10:54:46 +0100

    Aggiustato feed

--- bussola.pl
+++ bussola.pl
@@ -1,11 +1,28 @@
 #!/usr/bin/env perl
 
 use Serchio;
+use Data::Dumper;
 
 use strict;
 use warnings;
 
-setdata "2013-10-10";
+setdata '2013-10-10';
+my ($former, $latter) = (feed, feed);
 
-#storeall "store.dat";
-unstoreall "store.dat";
+if( $former && $latter ){
+	my $current;
+	do{
+		if( $current ne $former->{utente}){
+			$current = $former->{utente};
+			<STDIN>;
+		}	
+
+		print "$former->{utente} - $former->{ora} - $former->{direzione}";
+		if(same $former, $latter){
+			print " | Doppia Badgiatura ($latter->{ora} - $latter->{direzione})\n";
+		}else{
+			print "\n";
+		}
+		($former, $latter) = (feed, $former);
+	}while($former);
+}
