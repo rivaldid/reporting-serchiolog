38a1ad517018dda3bd9f764e917a0a5f84308e7f
Dario Vilardi <vilardid@posteitaliane.com>
2016-07-08 13:42:19 +0200

    cleanup

--- commit2db.sh
+++ commit2db.sh
@@ -1,17 +1,19 @@
 #!/usr/local/bin/bash
 
-MOUNTPOINT=/mnt/13/
-RSYNCFROM=DC/REPORT/Export/Serchio/
-RSYNCTO=/root/script/serchiolog/xpsbucket
+MOUNTPOINT="/mnt/13/"
+RSYNCFROM="DC/REPORT/Export/Serchio/"
+RSYNCTO="/root/script/serchiolog/xpsbucket"
+MONTA13="/root/script/monta13.sh"
 
 BINCOPPIE=/root/script/serchiolog/generacoppie.pl
+BINBASH=/usr/local/bin/bash
 
 #SAVEIFS=${IFS}
 #IFS=;
 
 # MONTA LA 13 SU RSYNCFROM
 
-/root/script/monta13 ${MOUNTPOINT}
+$BINBASH $MONTA13 $MOUNTPOINT
 
 # RSYNC IN RYNCTO
 
@@ -20,14 +22,14 @@ if [ $? -eq 0 ]; then
 	/bin/echo "==========> RSYNC 13 e DB 159"
 	
 	# BUTTA IN DB
-	for item in  $(/usr/local/bin/rsync -r --exclude '* *' --ignore-existing --out-format "%n" ${MOUNTPOINT}${RSYNCFROM} ${RSYNCTO} |grep xps)
+	for item in  $(/usr/local/bin/rsync -r --delete --exclude '* *' --ignore-existing --out-format "%n" ${MOUNTPOINT}${RSYNCFROM} ${RSYNCTO} |grep xps)
 	do
 		/bin/echo "----------> serchiolog: processing file $item"
 		/root/script/serchiolog/serchiolog.pl ${RSYNCTO}/$item # get things done
 	done
 
 	/bin/echo "==========> Ok, ho fatto. Smonto e ciao."
-	/sbin/umount ${MOUNTPOINT}
+	/sbin/umount $MOUNTPOINT
 	
 	/bin/echo "==========> aspe, mo faccio una cosa che serve a dario"
 	/usr/bin/perl ${BINCOPPIE}
